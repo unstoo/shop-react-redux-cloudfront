de351e6027480a0e6cba09741e8beb011e265618
Jevgenijs Kozlovs <jevgenijs_kozlovs@epam.com>
2022-07-04 14:30:41 +0300

    feat: integrate with import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://4dygu0sk4d.execute-api.eu-central-1.amazonaws.com/',
+  order: '',
+  import: 'https://7vnvssamh3.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://4dygu0sk4d.execute-api.eu-central-1.amazonaws.com/',
+  cart: '',
 };
 
 export default API_PATHS;
